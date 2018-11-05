f88bdf2675dfb0f34b2d555ca8b508f5856343eb
HeyItsMeNobody <blomdyon@yahoo.com>
2018-11-05 20:28:33 +0100

    Added on and off button when right click on a page

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,30 +1,70 @@
-setInterval(function() {
-    try {
-        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-            var url = tabs[0].url;
-            
-            var step1 = url.replace("http://", "");
-            var step2 = step1.replace("https://", "");
-            var finish = step2.replace("www.", "");
-            finish = finish.substring(0, finish.indexOf('/')+1).replace('/', '');
-                
-            xhr = new XMLHttpRequest();
-            var url = "http://localhost:9999/api/chrome";
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = function () { 
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    var json = JSON.parse(xhr.responseText);
-                    console.log(`RemChrome: Received message from localhost back: ${json.message}`)
-                }
-            }
-            var data = JSON.stringify({"details":`${finish}`,"state":"N/A", "largeImageKey":"chrome"});
-            xhr.send(data);
-            
-            console.log(`RemChrome: Send RPC to http://localhost:9999/api/chrome with data ${data}`);
-        });
+var contextMenuControlPanel = {
+    "id": "controlpanel",
+    "title": "Control Panel",
+    "contexts": ["all"]
+};
+var contextMenuOn = {
+    "id": "on",
+    "title": "On",
+    "contexts": ["all"]
+};
+var contextMenuOff = {
+    "id": "off",
+    "title": "Off",
+    "contexts": ["all"]
+};
+Toggle = true;
+
+chrome.contextMenus.create(contextMenuControlPanel);
+chrome.contextMenus.create(contextMenuOn);
+chrome.contextMenus.create(contextMenuOff);
+
+chrome.contextMenus.onClicked.addListener(function(contextMenuClick) {
+    if (contextMenuClick.menuItemId == "controlpanel") {
+        chrome.tabs.create({ url: "http://localhost:9999/" });
     }
-    catch (e) {
-        console.log(`Error: ${e}`)
+    if (contextMenuClick.menuItemId == "on") {
+        Toggle = true;
+    }
+    if (contextMenuClick.menuItemId == "off") {
+        Toggle = false;
+    }
+});
+
+setInterval(function() {
+    if (Toggle == true) {
+        try {
+            chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+                var url = tabs[0].url;
+                
+                var step1 = url.replace("http://", "");
+                var step2 = step1.replace("https://", "");
+                var finish = step2.replace("www.", "");
+                finish = finish.substring(0, finish.indexOf('/')+1).replace('/', '');
+    
+                xhr = new XMLHttpRequest();
+                var url = "http://localhost:9999/api/chrome";
+                xhr.open("POST", url, true);
+                xhr.setRequestHeader("Content-type", "application/json");
+                xhr.onreadystatechange = function () { 
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        var json = JSON.parse(xhr.responseText);
+                        console.log(`RemChrome: Received message from localhost back: ${json.message}`)
+                    }
+                }
+    
+                if(finish == "youtube.com") {
+                    var data = JSON.stringify({"details":`${finish}`,"state":"Video: N/A", "largeImageKey":"chrome"});
+                } else {
+                    var data = JSON.stringify({"details":`${finish}`,"state":"N/A", "largeImageKey":"chrome"});
+                }
+    
+                xhr.send(data);
+                console.log(`RemChrome: Send RPC to http://localhost:9999/api/chrome with data ${data}`);
+            });
+        }
+        catch (e) {
+            console.log(`Error: ${e}`)
+        }
     }
 }, 1000);
